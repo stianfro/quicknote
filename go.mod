65b593a1f80bd6b7e5cae5b806c4d0d460507047
Stian Frøystein <stianfroy@gmail.com>
2024-04-18 09:43:20 +0900

    add script

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/stianfro/quicknote
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+type Properties struct {
+	ID      string   `json:"id"`
+	Aliases []string `json:"aliases"`
+	Tags    []string `json:"tags"`
+	Date    string   `json:"date"`
+}
+
+func main() {
+	timeStamp := time.Now()
+	noteID := "quicknote-" + fmt.Sprint(timeStamp.Unix())
+
+	note := fmt.Sprintf(`---
+id: %s
+tags:
+  - quicknote
+date: "%s"
+---
+
+`, noteID, timeStamp.Format("2006-01-02"))
+
+	filePath := os.Getenv("HOME") + "/notes/quicknotes/" + noteID + ".yaml"
+
+	err := os.WriteFile(filePath, []byte(note), 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(filePath)
+}
